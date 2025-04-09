945bde820765c034b269f8f18843475862f47d58
ab.ch <mikaelabechristanto@gmail.com>
2025-04-10 00:23:30 +0700

    Create topo3swch.py

---
+++ topo3swch.py
@@ -0,0 +1,23 @@
+from mininet.topo import Topo
+
+class CustomTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(h3, s3)
+        self.addLink(h4, s3)
+        self.addLink(h5, s3)
+
+topos = {'mytopo': (lambda: CustomTopo())}
